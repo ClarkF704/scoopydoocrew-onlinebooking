4defee055e6c409dae83b2bd41398de64bf3b02e
Clark Farriss <clarkfarriss@Clarks-MBP.lan>
2022-02-17 16:53:11 -0500

    changing from test to prod

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require('express');
-const stripe = require('stripe')(process.env.TEST_PRI_KEY);
+const stripe = require('stripe')(process.env.PROD_PRI_KEY);
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
@@ -40,7 +40,7 @@ app.get('/contact', (req, res) => {
 
 app.get('/scheduler', (req, res) => {
   res.render('scheduler', {
-    stripePublishableKey: process.env.TEST_PUB_KEY,
+    stripePublishableKey: process.env.PROD_PUB_KEY,
   });
 });
 
